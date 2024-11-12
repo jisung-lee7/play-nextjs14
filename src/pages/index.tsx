c7eb850657f8e24812395cbf032b360bbe4ef050
Jisung Lee <jskurt9352@gmail.com>
2024-11-12 11:36:30 +0900

    feat: add Server Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,40 @@
 import SearchableLayout from '@/components/searchable-layout'
 import style from './index.module.css'
 import { ReactNode } from 'react'
-import books from '@/mock/books.json'
 import BookItem from '@/components/book-item'
+import { InferGetServerSidePropsType } from 'next'
+import fetchBooks from '@/lib/fetch-books'
+import fetchRandomBooks from '@/lib/fetch-random-books'
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks()
+  ])
+
+  return {
+    props: {
+      allBooks,
+      recoBooks
+    }
+  }
+}
+
+export default function Home({
+  allBooks,
+  recoBooks
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>Current recommended books</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>All registered books</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
